bb9448bd10c4979a583b7fc884c92ebe5dd5453c
shawn <shawnsagiv@gmail.com>
2021-12-02 19:10:21 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/shawn_personal.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/shawn_personal.dir/main.cpp.o"
+  "shawn_personal"
+  "shawn_personal.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/shawn_personal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/shawn_personal.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for shawn_personal.
+# This may be replaced when dependencies are built.
